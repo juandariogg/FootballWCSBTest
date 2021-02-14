c3f587e9f4bf4836e57d07182cb598608673d850
Juanikus <juanikus99@gmail.com>
2021-02-14 20:50:52 +0100

    New tests

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FootballWCSBTests
 Unit Testings for FootbalWCSB project
+
+<hr>
+
+These are the tests for the project FootballWCSB, which can be found [here](https://github.com/juandariogg/FootballWCSB)
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,16 +14,25 @@ namespace Test
         {
             var matchManager = new MatchManager();
 
-        }
+            var match = matchManager.StartMatch("Spain", "United Kingdom");
 
-        [TestMethod]
-        [ExpectedException(typeof(Exception), "There is already a match running for these teams")]
-        public void DoubleStartTest()
-        {
-            var matchManager = new MatchManager();
+            match = matchManager.UpdateScore(match, 1, null);
 
-            matchManager.StartMatch("Spain", "Italy");
-            matchManager.StartMatch("Spain", "Italy");
+            match = matchManager.UpdateScore(match, null, 1);
+
+            match = matchManager.UpdateScore(match, null, 2);
+
+            var summaryStringList = matchManager.GetSummary().Select(x => x.ToString());
+            var summary = string.Join("\n", summaryStringList);
+            var expectedSummary = "Uruguay 6 - Italy 6\nSpain 10 - Brazil 2\nMexico 0 - Canada 5\nArgentina 3 - Australia 1\nGermany 2 - France 2\nSpain 1 - United Kingdom 2";
+            Assert.AreEqual(summary, expectedSummary);
+
+            matchManager.FinishMatch(match);
+
+            summaryStringList = matchManager.GetSummary().Select(x => x.ToString());
+            summary = string.Join("\n", summaryStringList);
+            expectedSummary = "Uruguay 6 - Italy 6\nSpain 10 - Brazil 2\nMexico 0 - Canada 5\nArgentina 3 - Australia 1\nGermany 2 - France 2";
+            Assert.AreEqual(summary, expectedSummary);
         }
 
         [TestMethod]
@@ -40,13 +49,46 @@ namespace Test
         }
 
         [TestMethod]
+        [ExpectedException(typeof(Exception), "There is already a match running for these teams")]
+        public void DoubleStartTest()
+        {
+            var matchManager = new MatchManager();
+
+            matchManager.StartMatch("Spain", "Italy");
+            matchManager.StartMatch("Spain", "Italy");
+        }
+
+        [TestMethod]
         [ExpectedException(typeof(Exception), "The provided match does not exist in the scoreboard")]
-        public void UpdateUnexistentMatchTest()
+        public void UpdateNonExistentMatchTest()
         {
             var matchManager = new MatchManager();
 
             var match = new Match("Portugal", "Brazil");
             matchManager.UpdateScore(match, 7, 5);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Can't set a negative score")]
+        public void UpdateSetNegativeScoreTest()
+        {
+            var matchManager = new MatchManager();
+
+            var match = matchManager.StartMatch("Spain", "Portugal");
+
+            matchManager.UpdateScore(match, -1, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "There is no match running for the specified teams")]
+        public void FinishNonExistentMatchTest()
+        {
+            var matchManager = new MatchManager();
+
+            var match = new Match("Portugal", "Brazil");
+            matchManager.FinishMatch(match);
+        }
+
+
     }
 }
